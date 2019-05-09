6044b4b0004dec15ff501dfaad4c7b13ed64725a
emil2709 <emil.nedregard@gmail.com>
2019-05-09 09:58:28 +0000

    change

--- process_over_time.sh
+++ process_over_time.sh
@@ -2,8 +2,9 @@
 
 for i in {1..1440}
 do
-	date +%T >> process.txt
-	(time -p ./3min_process.out) |& awk 'FNR == 2{print $2}' >> process.txt &
+	date +%T >> cpuvendor.txt
+	echo "$1" >> cpuvendor.txt
+	(time -p ./a.out) |& awk 'FNR == 2{print $2}' >> cpuvendor.txt &
 	sleep 60
 done
 
